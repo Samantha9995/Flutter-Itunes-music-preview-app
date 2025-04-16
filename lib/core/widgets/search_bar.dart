a30d040ed860fc03a5182293428837881548329b
SADev - MAC <47018729+Samantha9995@users.noreply.github.com>
2025-04-16 17:27:09 +0800

    add list under TextField

--- lib/core/widgets/search_bar.dart
+++ lib/core/widgets/search_bar.dart
@@ -3,7 +3,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:itunes_music_app/core/utils/constants.dart';
 import 'package:itunes_music_app/features/search/controllers/search_controller.dart';
 
-class SearchMusicBar extends StatelessWidget {
+class SearchMusicBar extends StatefulWidget {
   const SearchMusicBar(
       {super.key, required this.controller, required this.onSearchTextChanged});
 
@@ -11,21 +11,66 @@ class SearchMusicBar extends StatelessWidget {
   final ValueChanged<String> onSearchTextChanged;
 
   @override
+  State<SearchMusicBar> createState() => _SearchMusicBarState();
+}
+
+class _SearchMusicBarState extends State<SearchMusicBar> {
+  
+  final FocusNode _focusNode = FocusNode();
+  bool _showSuggestions = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+
+      setState(() {
+        _showSuggestions = _focusNode.hasFocus;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(defaultPadding),
-      child: TextField(
-        decoration: InputDecoration(
-          hintText: context.tr('search_music'),
-          prefixIcon: const Icon(Icons.search),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(borderRadius),
+      child: Column(
+        children: [
+          TextField(
+            focusNode: _focusNode,
+            decoration: InputDecoration(
+              hintText: context.tr('search_music'),
+              prefixIcon: const Icon(Icons.search),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(borderRadius),
+              ),
+            ),
+            onChanged: (value) {
+              widget.onSearchTextChanged(value);
+            },
+            style: Theme.of(context).textTheme.bodyMedium,
           ),
-        ),
-        onChanged: (value) {
-          onSearchTextChanged(value);
-        },
-        style: Theme.of(context).textTheme.bodyMedium,
+          if (_showSuggestions)
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(borderRadius),
+                border: Border.all(color: Colors.grey.shade300),
+              ),
+              child: ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text("$index"),
+                    onTap: () {
+                    },
+                  );
+                },
+              ),
+            ),
+        ],
       ),
     );
   }
