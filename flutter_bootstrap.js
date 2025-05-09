0be956bfe60667299f8c0848cde7f4843feedc6d
Samantha9995 <Samantha9995@users.noreply.github.com>
2025-05-09 09:28:43 +0000

    deploy: 461e3d84d1ca604004a497fb97b579fe4cba0331

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "674332091"
+    serviceWorkerVersion: "1938203976"
   }
 });
